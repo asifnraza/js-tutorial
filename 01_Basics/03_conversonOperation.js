716d7be5668eeb4f957be8530d142f925c5b6237
Asif Raza <47889701+asifnraza@users.noreply.github.com>
2023-08-01 18:54:25 +0000

    conversion of data types

---
+++ 01_Basics/03_conversonOperation.js
@@ -0,0 +1,30 @@
+let score = "asif"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let convertInNumber = Number(score)
+
+// console.log(convertInNumber);
+// console.log(typeof convertInNumber);
+
+// "33" - 33
+// "33abc" - NaN - not a number
+// true - 1; flase - 0
+
+let isLoggedIn = 0
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1 - true; 0 - false
+// "" - false
+// "Asif" - true
+
+let someNum = 33
+let stringNumber = String(someNum)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
