7602d0ebcbdf3d9ab697c7779a0238d5cc2d511b
Asif Raza <47889701+asifnraza@users.noreply.github.com>
2023-08-01 18:26:45 +0000

    devcontainer config and datatypes

---
+++ 01_Basics/01_var.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "asif@gmail.com"
+var accountPass = "123123"
+
+accountCity = "Delhi"
+
+let accountStatus;
+
+// accountId = 2 cant assign value in const
+/*
+prefer not to use var
+becouse of issue in scope and block like { code statement }
+*/
+
+console.table([accountEmail, accountId, accountPass, accountCity, accountStatus])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"user strict" // treat js code as all newer version
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(3 + 4); 
+console.log("Asif")
+
+// code readability should be high
+
+
+let name = "Asif"
+let age = 30
+let isLoggedIn = false
+let state
+
+// primitive data types
+
+// number -> 2 to the power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined - value needs to be assign in the data types
+// symboles = unique
+
+// object = 
