5d66b484365bfdd4ac64956fc8cd5b3d4541c16c
Asif Raza <47889701+asifnraza@users.noreply.github.com>
2023-08-04 17:34:34 +0000

    num n math

---
+++ 01_Basics/07_num_math.js
@@ -0,0 +1,38 @@
+const myNumber = 400
+// console.log(myNumber);
+
+// const newNum = new Number(300)
+// console.log(newNum);
+
+// console.log(newNum.toString().length);
+// console.log(newNum.toFixed(2));
+
+const rup = 20000000
+
+// console.log(rup.toLocaleString('en-IN'));
+
+
+const preNum = 23.893
+
+// console.log(preNum.toPrecision(1));
+
+// ------------------Math----------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-2))
+// console.log(Math.PI);
+// console.log(Math.round(4.8))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(4, 3,2,4,514,5,1,3))
+// console.log(Math.max(4, 3,2,4,514,5,1,3))
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 50
+const max = 125
+
+console.log(Math.floor(Math.random()*(max - min)+1) + min)
+
