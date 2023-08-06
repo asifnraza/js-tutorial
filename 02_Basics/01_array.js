770b700bdb2705a78f1227b8f5f0d06fabd40c32
Asif Raza <47889701+asifnraza@users.noreply.github.com>
2023-08-06 11:28:14 +0000

    array basics

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5, 6]
+const myHeros = ["Superman", "Ironman"]
+const newArr = new Array(1,2,3,4,5,6)
+// console.log(myArr[3]);
+
+// array methods
+
+// myArr.push(8)
+// myArr.push(89)
+// myArr.pop()
+
+// myArr.unshift(4)
+// myArr.shift()
+
+// console.log(myArr.includes(1));
+// console.log(myArr.indexOf(3));
+
+const newArray = myArr.join()
+
+// console.log(myArr);
+// console.log(newArray);
+
+// slice/splice
+
+console.log("A ", myArr);
+const newA1 = myArr.slice(1,3)
+console.log(newA1);
+
+
+console.log("B ", myArr);
+const newA2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(newA2);
+
+
+
+
