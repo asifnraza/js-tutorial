f5d3078d265dcbd1d9a5e9e3ec6822f40002936e
Asif Raza <47889701+asifnraza@users.noreply.github.com>
2023-08-16 16:48:26 +0000

    while dowhile loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// let index = 0;
+
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`);  
+//     index = index + 2;
+// }
+
+
+// let myArr = ["Superman", "Batman", "DC"]
+// let arr = 0;
+// while (arr < myArr.length) {
+//     console.log(`Values of Array ${myArr[arr]}`);
+//     arr++;
+    
+// }
+
+
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <=10);
